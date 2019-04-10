4040ded892ad787adede71fb2f2adb38cbbca766
duj <duj@yillion.dev>
2019-04-10 18:50:28 +0800

    类加载

--- src/main/java/com/dj/proxy/JdkProxyClassLloader.java
+++ src/main/java/com/dj/proxy/JdkProxyClassLloader.java
@@ -1,4 +1,64 @@
 package com.dj.proxy;
 
-public class JdkProxyClassLloader {
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * 默认ClassLoader，只加载指定目录下的jar和class，如果我们想加载其它位置的类或jar时，比如：我要加载网络上的一个class文件，通过动态加载到内存之后，要调用这个类中的方法实现我的业务逻辑。在这样的情况下，默认的ClassLoader就不能满足我们的需求了，所以需要定义自己的ClassLoader。
+ *
+ * 定义自已的类加载器分为两步：
+ *
+ * 1、继承java.lang.ClassLoader
+ *
+ * 2、重写父类的findClass方法
+ *
+ * 因为JDK已经在loadClass方法中帮我们实现了ClassLoader搜索类的算法，当在loadClass方法中搜索不到类时，loadClass方法就会调用findClass方法来搜索类，所以我们只需重写该方法即可。如没有特殊的要求，一般不建议重写loadClass搜索类的算法。
+ */
+public class JdkProxyClassLloader extends ClassLoader{
+    private File classPathFile;
+    public JdkProxyClassLloader(){
+        String classPath = JdkProxyClassLloader.class.getResource("").getPath();
+        this.classPathFile = new File(classPath);
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String className = JdkProxyClassLloader.class.getPackage().getName() + "." + name;
+        if(classPathFile != null){
+            File classFile = new File(classPathFile,name.replaceAll("\\.","/") + ".class");
+            if(classFile.exists()){
+                FileInputStream in = null;
+                ByteArrayOutputStream out = null;
+                try{
+                    in = new FileInputStream(classFile);
+                    out = new ByteArrayOutputStream();
+                    byte [] buff = new byte[1024];
+                    int len;
+                    while ((len = in.read(buff)) != -1){
+                        out.write(buff,0,len);
+                    }
+                    return defineClass(className,out.toByteArray(),0,out.size());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(null != in){
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }if(out != null){
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
